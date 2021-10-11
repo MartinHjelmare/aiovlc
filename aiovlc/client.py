560230dc4b3fc87d97792f2110154af22be3f632
Martin Hjelmare <marhje52@gmail.com>
2021-10-12 00:34:11 +0200

    Add client command helpers (#18)

--- aiovlc/client.py
+++ aiovlc/client.py
@@ -4,9 +4,33 @@ from __future__ import annotations
 import asyncio
 import re
 from types import TracebackType
+from typing import Literal
 
 from .const import LOGGER
 from .exceptions import AuthError, CommandError, ConnectError, ConnectReadError
+from .model.command import (
+    Add,
+    Clear,
+    Enqueue,
+    GetLength,
+    GetLengthOutput,
+    GetTime,
+    GetTimeOutput,
+    Info,
+    InfoOutput,
+    Next,
+    Pause,
+    Play,
+    Prev,
+    Random,
+    Seek,
+    SetVolume,
+    Status,
+    StatusOutput,
+    Stop,
+    Volume,
+    VolumeOutput,
+)
 
 IAC = bytes([255])  # "Interpret As Command"
 TERMINATOR = "\n"
@@ -14,6 +38,8 @@ TERMINATOR = "\n"
 
 class Client:
     """Represent a client for aiovlc."""
+
+    # pylint: disable=too-many-public-methods
 
     def __init__(
         self, password: str, host: str = "localhost", port: int = 4212
@@ -118,3 +144,67 @@ class Client:
                 raise CommandError(command_error.group())
 
         return command_output
+
+    async def add(self, playlist_item: str) -> None:
+        """Send the add command."""
+        await Add(playlist_item).send(self)
+
+    async def clear(self) -> None:
+        """Send the clear command."""
+        await Clear().send(self)
+
+    async def enqueue(self, playlist_item: str) -> None:
+        """Send the enqueue command."""
+        await Enqueue(playlist_item).send(self)
+
+    async def get_length(self) -> GetLengthOutput:
+        """Send the get_length command."""
+        return await GetLength().send(self)
+
+    async def get_time(self) -> GetTimeOutput:
+        """Send the get_time command."""
+        return await GetTime().send(self)
+
+    async def info(self) -> InfoOutput:
+        """Send the info command."""
+        return await Info().send(self)
+
+    async def next(self) -> None:
+        """Send the next command."""
+        await Next().send(self)
+
+    async def pause(self) -> None:
+        """Send the pause command."""
+        await Pause().send(self)
+
+    async def play(self) -> None:
+        """Send the play command."""
+        await Play().send(self)
+
+    async def prev(self) -> None:
+        """Send the prev command."""
+        await Prev().send(self)
+
+    async def random(self, mode: Literal["on", "off"] | None = None) -> None:
+        """Send the random command."""
+        await Random(mode).send(self)
+
+    async def seek(self, seconds: int) -> None:
+        """Send the seek command."""
+        await Seek(seconds).send(self)
+
+    async def set_volume(self, volume: int) -> None:
+        """Send the volume command with a parameter to set volume."""
+        await SetVolume(volume).send(self)
+
+    async def status(self) -> StatusOutput:
+        """Send the status command."""
+        return await Status().send(self)
+
+    async def stop(self) -> None:
+        """Send the stop command."""
+        await Stop().send(self)
+
+    async def volume(self) -> VolumeOutput:
+        """Send the volume command."""
+        return await Volume().send(self)
