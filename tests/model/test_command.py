ee5f8027ce4b147640e5bd3b2027cb387c3366f7
Martin Hjelmare <marhje52@gmail.com>
2021-10-15 23:58:48 +0200

    Add test foundation (#22)
    * Add test foundation
    
    * Type it all

---
+++ tests/model/test_command.py
@@ -0,0 +1,34 @@
+"""Test the commands."""
+from __future__ import annotations
+
+from unittest.mock import AsyncMock, call
+
+import pytest
+
+from aiovlc.client import Client
+from aiovlc.model.command import Status
+
+# pylint: disable=unused-argument
+
+# All test coroutines will be treated as marked.
+pytestmark = pytest.mark.asyncio
+
+
+async def test_status_command(
+    transport: tuple[AsyncMock, AsyncMock],
+    client_connected: Client,
+    status_command_response: None,
+) -> None:
+    """Test the status command."""
+    mock_reader, mock_writer = transport
+
+    command = Status()
+    output = await command.send(client_connected)
+
+    assert mock_writer.write.call_count == 1
+    assert mock_writer.write.call_args == call(b"status\n")
+    assert mock_reader.readuntil.call_count == 1
+    assert output
+    assert output.audio_volume == 0
+    assert output.state == "stopped"
+    assert output.input_loc is None
