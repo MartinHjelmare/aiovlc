20a04b82d642393d45e17b968f25c49ac72f78bc
Martin Hjelmare <marhje52@gmail.com>
2022-08-04 18:13:08 +0200

    Add password test

--- tests/model/test_command.py
+++ tests/model/test_command.py
@@ -4,7 +4,7 @@ from __future__ import annotations
 from unittest.mock import AsyncMock, call
 
 from aiovlc.client import Client
-from aiovlc.model.command import Status
+from aiovlc.model.command import Password, Status
 
 # pylint: disable=unused-argument
 
@@ -27,3 +27,27 @@ async def test_status_command(
     assert output.audio_volume == 0
     assert output.state == "stopped"
     assert output.input_loc is None
+
+
+async def test_password_command(
+    transport: tuple[AsyncMock, AsyncMock],
+    client_connected: Client,
+) -> None:
+    """Test the password command."""
+    password = "test-password"
+    mock_reader, mock_writer = transport
+    mock_reader.readuntil.side_effect = [
+        b"VLC media player 3.0.9.2 Vetinari\nPassword: ",
+        b"\xff\xfb\x01\xff\xfc\x01\r\n",
+        b"Welcome, Master\r\n",
+        b"> ",
+    ]
+
+    command = Password(password)
+    output = await command.send(client_connected)
+
+    assert mock_writer.write.call_count == 1
+    assert mock_writer.write.call_args == call(f"{password}\n".encode())
+    assert mock_reader.readuntil.call_count == 4
+    assert output
+    assert output.response == "Welcome, Master"
