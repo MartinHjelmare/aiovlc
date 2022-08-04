32ffd01c3383ee0e55d93f3baa1bf7d395254f7b
Martin Hjelmare <marhje52@gmail.com>
2022-08-04 21:26:41 +0200

    Test prev command

--- tests/model/test_command.py
+++ tests/model/test_command.py
@@ -307,6 +307,22 @@ async def test_play_command(
     assert output is None
 
 
+async def test_prev_command(
+    transport: tuple[AsyncMock, AsyncMock],
+    client_connected: Client,
+) -> None:
+    """Test the prev command."""
+    mock_reader, mock_writer = transport
+    mock_reader.readuntil.return_value = b"> "
+
+    output = await client_connected.prev()
+
+    assert mock_writer.write.call_count == 1
+    assert mock_writer.write.call_args == call(b"prev\n")
+    assert mock_reader.readuntil.call_count == 1
+    assert output is None
+
+
 async def test_status_command(
     transport: tuple[AsyncMock, AsyncMock],
     client_connected: Client,
